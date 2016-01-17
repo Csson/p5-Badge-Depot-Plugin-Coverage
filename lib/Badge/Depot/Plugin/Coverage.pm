4848a8b1a1d2ffd97ab18660bc0791f6fc491491
Erik Carlsson <info@code301.com>
2016-01-17 13:39:37 +0000

    Init

---
+++ lib/Badge/Depot/Plugin/Coverage.pm
@@ -0,0 +1,28 @@
+use 5.14.0;
+use strict;
+use warnings;
+
+package Badge::Depot::Plugin::Coverage;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot::Plugin::Coverage;
+
+=head1 DESCRIPTION
+
+Badge::Depot::Plugin::Coverage is ...
+
+=head1 SEE ALSO
+
+=cut
